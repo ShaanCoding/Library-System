e2d59d86e52651cf80cba89fa327858323345136
Shaan Khan <shaankhan101@gmail.com>
2020-10-13 23:26:07 +1100

    Main menu is at least partially done

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -1,8 +1,11 @@
 package controller;
 
 import au.edu.uts.ap.javafx.Controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import model.Library;
+
+import javax.swing.*;
 
 public class LibraryController extends Controller<Library> {
 
@@ -11,8 +14,28 @@ public class LibraryController extends Controller<Library> {
 
     }
 
+    @FXML private void exploreCatalogue(ActionEvent event) {
+
+    }
+
+    @FXML private void patronRecord(ActionEvent event) {
+
+    }
+
+    @FXML private void favouriteBooks(ActionEvent event) {
+
+    }
+
+    @FXML private void administrationMode(ActionEvent event) {
+
+    }
+
+    @FXML private void exit(ActionEvent event) {
+        //Exits the application
+        stage.close();
+    }
+
     public final Library getLibrary() {
         return model;
     }
-
 }
