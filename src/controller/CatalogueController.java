964b4df4a52910dbca408324739ede5290ccfd48
Shaan Khan <shaankhan101@gmail.com>
2020-10-15 10:58:37 +1100

    The catalogue menu is at least partially done

--- src/controller/CatalogueController.java
+++ src/controller/CatalogueController.java
@@ -18,31 +18,66 @@ public class CatalogueController extends Controller<Library>
     }
 
     @FXML private void allBooks(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/showAllBooks.fxml", "Complete Catalogue", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void availableBooks(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/showAvailableBooks.fxml", "Available Books", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void booksByGenre(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/showBooksByGenre.fxml", "Browse by Genre", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void booksByAuthor(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/showBooksByAuthor.fxml", "Browse by Author", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void borrowBooks(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/borrow.fxml", "Borrow a Book", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void returnBooks(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/return.fxml", "Return a Book", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void placeHold(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/placeHold.fxml", "Place a Hold", stage);
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void exitCatalogue(ActionEvent actionEvent) {
