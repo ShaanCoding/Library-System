3805f92d6db9b76a22b5c2ec2c85c150b8e696d8
Shaan Khan <shaankhan101@gmail.com>
2020-10-15 14:11:07 +1100

    The Add Book Is At Least Partially Done

--- src/controller/AddBookController.java
+++ src/controller/AddBookController.java
@@ -1,5 +1,46 @@
 package controller;
 
-public class AddBookController {
+import au.edu.uts.ap.javafx.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import model.Library;
 
+
+public class AddBookController  extends Controller<Library>
+{
+    @FXML private TextField titleTf;
+    @FXML private TextField authorTf;
+    @FXML private TextField genreTf;
+
+    public String getTitle() {
+        return titleTf.getText();
+    }
+
+    public String getAuthor() {
+        return authorTf.getText();
+    }
+
+    public String getGenre() {
+        return genreTf.getText();
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public final Library getLibrary() {
+        return model;
+    }
+
+    @FXML private void addBook(ActionEvent actionEvent) {
+        //Adds book to library
+        getLibrary().getCatalogue().addBook(getTitle(), getAuthor(), getGenre());
+        //System.out.println(getLibrary().getCatalogue().getAllBooks().get(0).toString());
+    }
+
+    @FXML private void close(ActionEvent actionEvent) {
+        stage.close();
+    }
 }
--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -4,6 +4,7 @@ import au.edu.uts.ap.javafx.Controller;
 import au.edu.uts.ap.javafx.ViewLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import model.Library;
 
 public class AdminController extends Controller<Library>
@@ -15,19 +16,39 @@ public class AdminController extends Controller<Library>
 
     @FXML
     private void addPatron(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/addPatron.fxml", "Add Patron", new Stage());
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void removePatron(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/removePatron.fxml", "Remove Patron", new Stage());
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void addBook(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/addBook.fxml", "Add Book", new Stage());
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void removeBook(ActionEvent actionEvent) {
+        try {
+            ViewLoader.showStage(getLibrary(), "/view/removeBook.fxml", "Remove Book", new Stage());
+        }
+        catch(Exception ex) {
 
+        }
     }
 
     @FXML private void exitAdministration(ActionEvent actionEvent) {
