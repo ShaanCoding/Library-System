6dbf8bb186935767ebb918e9781235bfca985372
Shaan Khan <shaankhan101@gmail.com>
2020-10-15 14:22:55 +1100

    Add Patron Is At Least Partially Done

--- src/controller/AddBookController.java
+++ src/controller/AddBookController.java
@@ -37,6 +37,9 @@ public class AddBookController  extends Controller<Library>
     @FXML private void addBook(ActionEvent actionEvent) {
         //Adds book to library
         getLibrary().getCatalogue().addBook(getTitle(), getAuthor(), getGenre());
+        titleTf.setText("");
+        authorTf.setText("");
+        genreTf.setText("");
         //System.out.println(getLibrary().getCatalogue().getAllBooks().get(0).toString());
     }
 
--- src/controller/AddPatronController.java
+++ src/controller/AddPatronController.java
@@ -1,5 +1,41 @@
 package controller;
 
-public class AddPatronController {
+import au.edu.uts.ap.javafx.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import model.Library;
 
+public class AddPatronController extends Controller<Library>
+{
+    @FXML private TextField patronIDTf;
+    @FXML private TextField patronNameTf;
+
+    public int getPatronID() {
+        return Integer.parseInt(patronIDTf.getText());
+    }
+
+    public String getPatronName() {
+        return patronNameTf.getText();
+    }
+
+    @FXML private void addPatron(ActionEvent actionEvent) {
+        //Adds patron to the list
+        getLibrary().addPatron(getPatronID(), getPatronName());
+        patronIDTf.setText("");
+        patronNameTf.setText("");
+    }
+
+    @FXML private void close(ActionEvent actionEvent) {
+        stage.close();
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public final Library getLibrary() {
+        return model;
+    }
 }
