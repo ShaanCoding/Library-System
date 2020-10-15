e912480c113c4eff69c0ee2cc3bf213e3096e6c6
Shaan Khan <shaankhan101@gmail.com>
2020-10-15 20:26:16 +1100

    Partially implemented the borrow book feature

--- src/controller/BorrowController.java
+++ src/controller/BorrowController.java
@@ -1,5 +1,61 @@
 package controller;
 
-public class BorrowController {
+import au.edu.uts.ap.javafx.Controller;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import model.Author;
+import model.Book;
+import model.Library;
+import model.Patron;
 
+public class BorrowController extends Controller<Library>
+{
+    @FXML private TextField patronTf;
+    @FXML private ListView availableBooksLv;
+
+    /*
+    Todo: replace with observable and update when user presses button
+     */
+
+    private int getPatronID() {
+        return Integer.parseInt(patronTf.getText());
+    }
+
+    private Book getSelectedBook() {
+        return (Book) availableBooksLv.getSelectionModel().getSelectedItem();
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public final Library getLibrary() {
+        return model;
+    }
+
+    @FXML private void selectPatron(ActionEvent actionEvent) {
+        //Checks if empty before casting
+        if(!patronTf.getText().isEmpty()) {
+            Patron patron = getLibrary().getPatron(getPatronID());
+            ObservableList<Book> bookObservableList = getLibrary().getCatalogue().getBorrowableBooks(patron);
+            availableBooksLv.setItems(bookObservableList);
+        }
+    }
+
+    @FXML private void borrowSelectedBook(ActionEvent actionEvent) {
+        if(getSelectedBook() != null) {
+            Book borrowedBook = getSelectedBook();
+            Patron patron = getLibrary().getPatron(getPatronID());
+
+            patron.borrowBook(borrowedBook);
+        }
+    }
+
+    @FXML private void close(ActionEvent actionEvent) {
+        stage.close();
+    }
 }
