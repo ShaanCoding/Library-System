899c71122ea5498070cfb33d35dabbb4409f8e9b
Shaan Khan <shaankhan101@gmail.com>
2020-10-15 15:53:38 +1100

    Partially completed the remove book section

--- src/controller/RemoveBookController.java
+++ src/controller/RemoveBookController.java
@@ -1,5 +1,41 @@
 package controller;
 
-public class RemoveBookController {
+import au.edu.uts.ap.javafx.Controller;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import model.Book;
+import model.Library;
 
-}
+public class RemoveBookController extends Controller<Library>
+{
+    @FXML
+    ListView booksLv;
+
+    public ObservableList<Book> getBooks() {
+        return getLibrary().getCatalogue().getAllBooks();
+    }
+
+    private Book getSelectedBooks() {
+        return (Book) booksLv.getSelectionModel().getSelectedItem();
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public final Library getLibrary() {
+        return model;
+    }
+
+    @FXML private void removeBook(ActionEvent actionEvent) {
+        if(getSelectedBooks() != null) {
+            getBooks().remove(getSelectedBooks());
+        }
+    }
+
+    @FXML private void close(ActionEvent actionEvent) {
+        stage.close();
+    }}
