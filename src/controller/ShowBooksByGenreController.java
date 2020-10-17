a83299d2fdef15b91908c8d87911d5354ee96a64
Shaan Khan <shaankhan101@gmail.com>
2020-10-17 16:54:21 +1100

    Show books by genre controller refactor

--- src/controller/ShowBooksByGenreController.java
+++ src/controller/ShowBooksByGenreController.java
@@ -24,25 +24,17 @@ public class ShowBooksByGenreController extends Controller<Library>
         return (Genre) genresLv.getSelectionModel().getSelectedItem();
     }
 
-    @FXML
-    public void initialize() {
-
-    }
-
-    public final Library getLibrary() {
-        return model;
-    }
-
     @FXML private void displayGenres(ActionEvent actionEvent) {
-        if(getSelectedGenre() != null) {
-            Genre genre = getSelectedGenre();
-            ObservableList<Book> booksInGenre = getLibrary().getCatalogue().getBooksInGenre(genre);
-            //System.out.println(booksInGenre.get(0));
-            booksInGenreLv.setItems(booksInGenre);
-        }
+        Genre genre = getSelectedGenre();
+        ObservableList<Book> booksInGenre = getLibrary().getCatalogue().getBooksInGenre(genre);
+        booksInGenreLv.setItems(booksInGenre);
     }
 
     @FXML private void close(ActionEvent actionEvent) {
         stage.close();
+    }
+
+    public final Library getLibrary() {
+        return model;
     }
 }
